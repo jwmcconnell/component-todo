b357b97a82ebce86b42efbaf6720cc3e11d11225
Jackmc <jackmc1122@gmail.com>
2019-05-21 20:36:05 -0700

    Add tests for TodoItem.

---
+++ test/TodoItem.test.js
@@ -0,0 +1,48 @@
+import TodoItem from '../src/components/TodoItem.js';
+const test = QUnit.test;
+
+QUnit.module('TodoItem');
+
+test('Checked TodoItem', assert => {
+  //Arrange
+  const todo = {
+    task: 'Learn templates',
+    completed: true
+  };
+
+  const expected = /*html*/`
+  <li>
+    <label>
+      <input type="checkbox" checked/>
+      Learn templates
+    </label>
+  </li>
+  `;
+  //Act 
+  const todoItem = new TodoItem({ todo });
+  const html = todoItem.renderTemplate();
+  //Assert
+  assert.htmlEqual(html, expected);
+});
+
+test('Unchecked TodoItem', assert => {
+  //Arrange
+  const todo = {
+    task: 'Learn forEach',
+    completed: false
+  };
+
+  const expected = /*html*/`
+  <li>
+    <label>
+      <input type="checkbox" />
+      Learn forEach
+    </label>
+  </li>
+  `;
+  //Act 
+  const todoItem = new TodoItem({ todo });
+  const html = todoItem.renderTemplate();
+  //Assert
+  assert.htmlEqual(html, expected);
+});
