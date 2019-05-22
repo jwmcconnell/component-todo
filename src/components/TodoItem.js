756c22ce3ff1135cddaee052379dfb3b4c4b1d62
Jackmc <jackmc1122@gmail.com>
2019-05-21 20:14:44 -0700

    Fix not checking for 'checked'.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,16 @@ class TodoItem extends Component {
   renderTemplate() {
     const todo = this.props.todo;
 
+    let checked = '';
+
+    if(todo.completed) {
+      checked = 'checked';
+    }
+
     return /*html*/`
     <li>
       <label>
-        <input type="checkbox" ${todo.checked}/>
+        <input type="checkbox" ${checked}/>
         ${todo.task}
       </label>
   </li>
