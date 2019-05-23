0cdd1177d02ec35224d4c23990b196433c0e199c
Jackmc <jackmc1122@gmail.com>
2019-05-22 19:40:34 -0700

    Connect filter search to App component.

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,11 @@ class App extends Component {
     const header = new Header();
     const headerDOM = header.render();
 
-    const filter = new Filter();
+    const filter = new Filter({
+      onFilter: (filter) => {
+        console.log(filter);
+      }
+    });
     const filterDOM = filter.render();
 
     const todoList = new TodoList({
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,8 +2,33 @@ import Component from './Component.js';
 
 class AddTodo extends Component {
 
+  render() {
+    const dom = this.renderDOM();
+
+    const form = dom.querySelector('form');
+
+    const onFilter = this.props.onFilter;
+
+    const filter = {
+      search: '',
+      status: ''
+    };
+
+    form.addEventListener('input', () => {
+      const formData = new FormData(form);
+
+      filter.search = formData.get('search');
+      filter.status = formData.get('status');
+
+      onFilter(filter);
+    });
+
+    return dom;
+  }
+
   renderTemplate() {
     return /*html*/`
+    <section>
       <form>
         <label>Search
           <input type="text" name="search"/>
@@ -12,19 +37,20 @@ class AddTodo extends Component {
         <legend>Filter Tasks</legend>
 
           <label>All
-            <input required name="state" value="all" type="radio" id="all">
+            <input required checked name="status" value="all" type="radio" id="all">
           </label>
 
           <label>Completed
-            <input required name="state" value="completed" type="radio" id="completed">
+            <input required name="status" value="completed" type="radio" id="completed">
           </label>
 
           <label>Not Done
-            <input required name="state" value="not-done" type="radio" id="not-done">
+            <input required name="status" value="not-done" type="radio" id="not-done">
           </label>
 
         </fieldset>
       </form>
+    </section>
     `;
   }
 
