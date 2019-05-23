6cb8dc8484b7d631965e51a3195a99f99382cece
Jackmc <jackmc1122@gmail.com>
2019-05-22 21:23:46 -0700

    Fix findById tab errors.

--- src/services/find-by-id.js
+++ src/services/find-by-id.js
@@ -1,11 +1,11 @@
 function findById(items, id) {
-    for(let i = 0; i < items.length; i++) {
-        let item = items[i];
-        if(item.id === id) {
-            return item;
-        }
+  for(let i = 0; i < items.length; i++) {
+    let item = items[i];
+    if(item.id === id) {
+      return item;
     }
-    return null;
+  }
+  return null;
 }
 
 export default findById;
